205556ab30cb069ab9e1ec3ed0df5025536c06b5
Fabien Lacorre <34058833+FabienLacorre@users.noreply.github.com>
2023-07-23 19:16:16 +0200

    feat: organize landing page

--- src/App.tsx
+++ src/App.tsx
@@ -7,50 +7,106 @@ import { Icon } from "./Atoms/Icon/Icon";
 import { Button } from "./Molecules/Button/Button";
 import { arrowLeftIcon } from "./Atoms/Icon/_Icons/arrowLeftIcon";
 
+const Buttons = () => {
+  return (
+    <>
+      <Typography size="xxlarge" weight="bold">
+        Buttons
+      </Typography>
+      <Box display="flex-row">
+        <Button
+          style={{ marginRight: 8 }}
+          content="GO BACK"
+          icon={<Icon size="medium" svg={arrowLeftIcon} />}
+        />
+        <Button content="SUBMIT" />
+      </Box>
+
+      <Box display="flex-row">
+        <Button
+          style={{ marginRight: 8 }}
+          content="GO BACK"
+          color="alert"
+          icon={<Icon size="medium" svg={arrowLeftIcon} />}
+        />
+        <Button content="SUBMIT" color="alert" />
+      </Box>
+
+      <Box display="flex-row">
+        <Button
+          style={{ marginRight: 8 }}
+          content="GO BACK"
+          color="error"
+          icon={<Icon size="medium" svg={arrowLeftIcon} />}
+        />
+        <Button content="SUBMIT" color="error" />
+      </Box>
+    </>
+  );
+};
+
+const Typographies = () => {
+  return (
+    <>
+      <Typography size="xxlarge" weight="bold">
+        Typographies
+      </Typography>
+      <Typography size="xlarge" weight="bold">
+        Size
+      </Typography>
+      <Box display="flex-column">
+        <Typography size="xxxlarge">xxxlarge: Lorem ipsum</Typography>
+        <Typography size="xxlarge">xxlarge: Lorem ipsum</Typography>
+        <Typography size="xlarge">xlarge: Lorem ipsum</Typography>
+        <Typography size="large">large: Lorem ipsum</Typography>
+        <Typography size="medium">medium: Lorem ipsum</Typography>
+        <Typography size="small">small: Lorem ipsum</Typography>
+      </Box>
+      <Typography size="xlarge" weight="bold">
+        Colors
+      </Typography>
+      <Box display="flex-column">
+        <Typography size="large" color="default">
+          Default: Lorem ipsum
+        </Typography>
+        <Typography size="large" color="primary">
+          Primary: Lorem ipsum
+        </Typography>
+        <Typography size="large" color="alert">
+          Alert: Lorem ipsum
+        </Typography>
+        <Typography size="large" color="error">
+          Error: Lorem ipsum
+        </Typography>
+      </Box>
+    </>
+  );
+};
+
+const Inputs = () => {
+  return (
+    <>
+      <Typography size="xxlarge" weight="bold">
+        Inputs
+      </Typography>
+      <Typography size="xlarge" weight="bold">
+        W.I.P
+      </Typography>
+      <Box>
+        <Input placeholder="this is a placeholder" />
+      </Box>
+    </>
+  );
+};
 function App() {
   return (
     <>
       <Header title="This is a header" boxColor="primary" textColor="white" />
       <Box padding="large">
         <Grid flow="row" gap="large">
-          <Box>
-            <Typography size="large" weight="xbold" color="error">
-              This is a Typography
-            </Typography>
-          </Box>
-
-          <Box display="flex-row">
-            <Button
-              style={{ marginRight: 8 }}
-              content="GO BACK"
-              icon={<Icon size="medium" svg={arrowLeftIcon} />}
-            />
-            <Button content="SUBMIT" />
-          </Box>
-
-          <Box display="flex-row">
-            <Button
-              style={{ marginRight: 8 }}
-              content="GO BACK"
-              color="alert"
-              icon={<Icon size="medium" svg={arrowLeftIcon} />}
-            />
-            <Button content="SUBMIT" color="alert" />
-          </Box>
-
-          <Box display="flex-row">
-            <Button
-              style={{ marginRight: 8 }}
-              content="GO BACK"
-              color="error"
-              icon={<Icon size="medium" svg={arrowLeftIcon} />}
-            />
-            <Button content="SUBMIT" color="error" />
-          </Box>
-
-          <Box>
-            <Input placeholder="this is a placeholder" />
-          </Box>
+          <Buttons />
+          <Typographies />
+          <Inputs />
         </Grid>
       </Box>
     </>
