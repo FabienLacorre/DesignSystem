88acf803caecd8496a24d8774a55b2de84a077c8
Fabien Lacorre <34058833+FabienLacorre@users.noreply.github.com>
2023-08-09 21:39:06 +0200

    feat: accordion shadow

--- src/Organisms/Accordion/Accordion.stories.tsx
+++ src/Organisms/Accordion/Accordion.stories.tsx
@@ -1,113 +1,185 @@
-
 import { type Meta, type StoryObj } from "@storybook/react";
-import { Accordion, } from "./Accordion";
+import { Accordion } from "./Accordion";
 import { useState } from "react";
+import { Box } from "../../Atoms/Box/Box";
 
 const meta = {
-    title: "Organisms/Accordion",
-    component: Accordion,
-    parameters: {
-        docs: {
-            description: {
-                component: "Accordion component"
-            },
-        },
+  title: "Organisms/Accordion",
+  component: Accordion,
+  parameters: {
+    docs: {
+      description: {
+        component: "Accordion component",
+      },
     },
-    tags: ["autodocs"],
+  },
+  tags: ["autodocs"],
 } satisfies Meta<typeof Accordion>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 const TemplateDefault = () => {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <>
-            <Accordion title="This is a title" isOpen={isOpen} onClick={() => {
-                setIsOpen(!isOpen)
-            }}>
-                This is a text in this accordion
-            </Accordion>
-        </>
-    )
-}
+  return (
+    <>
+      <Accordion
+        title="This is a title"
+        isOpen={isOpen}
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        This is a text in this accordion
+      </Accordion>
+    </>
+  );
+};
 export const Default: Story = {
-    args: {
-        title: "This is a title accordion",
-        isOpen: false,
-    },
-    render: (args) => <TemplateDefault />
-}
+  args: {
+    title: "This is a title accordion",
+    isOpen: false,
+  },
+  render: (args) => <TemplateDefault />,
+};
 
 const TemplateWithColors = () => {
-    const [isOpenPrimary, setIsOpenPrimary] = useState(false)
-    const [isOpenSuccess, setIsOpenSuccess] = useState(false)
-    const [isOpenWarning, setIsOpenWarning] = useState(false)
-    const [isOpenError, setIsOpenError] = useState(false)
+  const [isOpenPrimary, setIsOpenPrimary] = useState(false);
+  const [isOpenSuccess, setIsOpenSuccess] = useState(false);
+  const [isOpenWarning, setIsOpenWarning] = useState(false);
+  const [isOpenError, setIsOpenError] = useState(false);
 
-    const onClickPrimary = () => { setIsOpenPrimary(!isOpenPrimary) }
-    const onClickSuccess = () => { setIsOpenSuccess(!isOpenSuccess) }
-    const onClickWarning = () => { setIsOpenWarning(!isOpenWarning) }
-    const onClickError = () => { setIsOpenError(!isOpenError) }
+  const onClickPrimary = () => {
+    setIsOpenPrimary(!isOpenPrimary);
+  };
+  const onClickSuccess = () => {
+    setIsOpenSuccess(!isOpenSuccess);
+  };
+  const onClickWarning = () => {
+    setIsOpenWarning(!isOpenWarning);
+  };
+  const onClickError = () => {
+    setIsOpenError(!isOpenError);
+  };
 
-    return (
-        <>
-            <Accordion onClick={onClickPrimary} color="primary" isOpen={isOpenPrimary} title="Primary">
-                Text in accordion
-            </Accordion>
-            <Accordion onClick={onClickSuccess} color="success" isOpen={isOpenSuccess} title="Success">
-                Text in accordion
-            </Accordion>
-            <Accordion onClick={onClickWarning} color="warning" isOpen={isOpenWarning} title="Warning">
-                Text in accordion
-            </Accordion>
-            <Accordion onClick={onClickError} color="error" isOpen={isOpenError} title="Error">
-                Text in accordion
-            </Accordion>
-        </>
-    )
+  return (
+    <>
+      <Box marginBottom="small">
+        <Accordion
+          onClick={onClickPrimary}
+          color="primary"
+          isOpen={isOpenPrimary}
+          title="Primary"
+        >
+          Text in accordion
+        </Accordion>
+      </Box>
+      <Box marginBottom="small">
+        <Accordion
+          onClick={onClickSuccess}
+          color="success"
+          isOpen={isOpenSuccess}
+          title="Success"
+        >
+          Text in accordion
+        </Accordion>
+      </Box>
+      <Box marginBottom="small">
+        <Accordion
+          onClick={onClickWarning}
+          color="warning"
+          isOpen={isOpenWarning}
+          title="Warning"
+        >
+          Text in accordion
+        </Accordion>
+      </Box>
+      <Box marginBottom="small">
+        <Accordion
+          onClick={onClickError}
+          color="error"
+          isOpen={isOpenError}
+          title="Error"
+        >
+          Text in accordion
+        </Accordion>
+      </Box>
+    </>
+  );
 };
 
 export const Colors: Story = {
-    args: { title: "" },
-    render: (args) => (
-        <TemplateWithColors />
-    ),
-}
+  args: { title: "" },
+  render: (args) => <TemplateWithColors />,
+};
 
 const TemplateWithColorsWithBorder = () => {
-    const [isOpenPrimary, setIsOpenPrimary] = useState(false)
-    const [isOpenSuccess, setIsOpenSuccess] = useState(false)
-    const [isOpenWarning, setIsOpenWarning] = useState(false)
-    const [isOpenError, setIsOpenError] = useState(false)
+  const [isOpenPrimary, setIsOpenPrimary] = useState(false);
+  const [isOpenSuccess, setIsOpenSuccess] = useState(false);
+  const [isOpenWarning, setIsOpenWarning] = useState(false);
+  const [isOpenError, setIsOpenError] = useState(false);
 
-    const onClickPrimary = () => { setIsOpenPrimary(!isOpenPrimary) }
-    const onClickSuccess = () => { setIsOpenSuccess(!isOpenSuccess) }
-    const onClickWarning = () => { setIsOpenWarning(!isOpenWarning) }
-    const onClickError = () => { setIsOpenError(!isOpenError) }
+  const onClickPrimary = () => {
+    setIsOpenPrimary(!isOpenPrimary);
+  };
+  const onClickSuccess = () => {
+    setIsOpenSuccess(!isOpenSuccess);
+  };
+  const onClickWarning = () => {
+    setIsOpenWarning(!isOpenWarning);
+  };
+  const onClickError = () => {
+    setIsOpenError(!isOpenError);
+  };
 
-    return (
-        <>
-            <Accordion isBottomBorder onClick={onClickPrimary} color="primary" isOpen={isOpenPrimary} title="Primary">
-                Text in accordion
-            </Accordion>
-            <Accordion isBottomBorder onClick={onClickSuccess} color="success" isOpen={isOpenSuccess} title="Success">
-                Text in accordion
-            </Accordion>
-            <Accordion isBottomBorder onClick={onClickWarning} color="warning" isOpen={isOpenWarning} title="Warning">
-                Text in accordion
-            </Accordion>
-            <Accordion isBottomBorder onClick={onClickError} color="error" isOpen={isOpenError} title="Error">
-                Text in accordion
-            </Accordion>
-        </>
-    )
+  return (
+    <>
+      <Accordion
+        shadowed={false}
+        isBottomBorder
+        onClick={onClickPrimary}
+        color="primary"
+        isOpen={isOpenPrimary}
+        title="Primary"
+      >
+        Text in accordion
+      </Accordion>
+      <Accordion
+        shadowed={false}
+        isBottomBorder
+        onClick={onClickSuccess}
+        color="success"
+        isOpen={isOpenSuccess}
+        title="Success"
+      >
+        Text in accordion
+      </Accordion>
+      <Accordion
+        shadowed={false}
+        isBottomBorder
+        onClick={onClickWarning}
+        color="warning"
+        isOpen={isOpenWarning}
+        title="Warning"
+      >
+        Text in accordion
+      </Accordion>
+      <Accordion
+        shadowed={false}
+        isBottomBorder
+        onClick={onClickError}
+        color="error"
+        isOpen={isOpenError}
+        title="Error"
+      >
+        Text in accordion
+      </Accordion>
+    </>
+  );
 };
 
 export const ColorsWithBorder: Story = {
-    args: { title: "" },
-    render: (args) => (
-        <TemplateWithColorsWithBorder />
-    ),
-}
+  args: { title: "" },
+  render: (args) => <TemplateWithColorsWithBorder />,
+};
--- src/Organisms/Accordion/Accordion.tsx
+++ src/Organisms/Accordion/Accordion.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
-import { Box } from "../../Atoms/Box/Box"
-import { Typography } from "../../Atoms/Typography/Typography"
+import { Box } from "../../Atoms/Box/Box";
+import { Typography } from "../../Atoms/Typography/Typography";
 import { BasicProps } from "../../Atoms/_Types/basicProps";
 import "./accordion.css";
 import { useEffect, useState } from "react";
@@ -9,56 +9,73 @@ import { IoMdArrowDropright } from "react-icons/io";
 import { Color } from "../../Atoms/_Types/basicTypes";
 
 export interface AccordionProps extends BasicProps {
-    color?: Color,
-    title: string;
-    isOpen?: boolean;
-    onClick?: () => void;
-    isBottomBorder?: boolean;
+  color?: Color;
+  title: string;
+  isOpen?: boolean;
+  onClick?: () => void;
+  isBottomBorder?: boolean;
+  shadowed?: boolean;
 }
 
-export const Accordion = ({ color = "primary", title, children, isOpen = false, onClick, className, style, isBottomBorder = false }: AccordionProps) => {
-    // CLASSES
-    const accordionColorDivider = `accordion--classic--${color}-divider`;
-    const accordionIsBottomBorder = isBottomBorder === false ? `accordion--no-border--global` : "";
+export const Accordion = ({
+  color = "primary",
+  title,
+  children,
+  isOpen = false,
+  onClick,
+  className,
+  style,
+  isBottomBorder = false,
+  shadowed = true,
+}: AccordionProps) => {
+  // CLASSES
+  const accordionColorDivider = `accordion--classic--${color}-divider`;
+  const accordionIsBottomBorder =
+    isBottomBorder === false ? `accordion--no-border--global` : "";
+  const shadowedClass = shadowed ? `shadow--classic--black` : "";
+  // LOGICAL
+  const [iconRotationClass, setIconRotationClass] = useState("");
 
-    console.log("accordionIsBottomBorder", accordionIsBottomBorder)
+  useEffect(() => {
+    setIconRotationClass(isOpen ? "rotateNinety" : "rotateNinetyToDefault");
+  }, [isOpen]);
 
-    // LOGICAL
-    const [iconRotationClass, setIconRotationClass] = useState("");
+  return (
+    <Box className={clsx([className, shadowedClass])} style={style}>
+      {/* HEADER */}
 
-    useEffect(() => {
-        setIconRotationClass(isOpen ? "rotateNinety" : "rotateNinetyToDefault")
-    }, [isOpen])
+      <Box
+        display="flex-row"
+        alignItems="center"
+        paddingBottom="small"
+        paddingTop="small"
+        className={clsx([
+          "accordion--classic--title-container",
+          `accordion--classic--${color}-title-container`,
+          accordionIsBottomBorder,
+          accordionColorDivider,
+        ])}
+        onClick={onClick}
+      >
+        <IoMdArrowDropright
+          className={clsx([
+            "font--size--large",
+            `accordion--classic--${color}-icon`,
+            iconRotationClass,
+          ])}
+        />
+        <Typography userSelect={false} weight="bold">
+          {title}
+        </Typography>
+      </Box>
 
-    return (
-        <Box className={className} style={style}>
+      {/* CONTENT */}
 
-            {/* HEADER */}
-
-            <Box display="flex-row" alignItems="center" paddingBottom="small" paddingTop="small"
-                className={clsx([
-                    "accordion--classic--title-container",
-                    `accordion--classic--${color}-title-container`,
-                    accordionIsBottomBorder,
-                    accordionColorDivider,
-                ])}
-                onClick={onClick}>
-                <IoMdArrowDropright
-                    className={clsx([
-                        "font--size--large",
-                        `accordion--classic--${color}-icon`,
-                        iconRotationClass,
-                    ])} />
-                <Typography userSelect={false} weight="bold">{title}</Typography>
-            </Box>
-
-            {/* CONTENT */}
-
-            <Collapse isOpen={isOpen}>
-                <Box paddingBottom="small" paddingTop="small">
-                    {children}
-                </Box>
-            </Collapse>
+      <Collapse isOpen={isOpen}>
+        <Box paddingBottom="small" paddingTop="small" paddingLeft="small">
+          {children}
         </Box>
-    )
-}
+      </Collapse>
+    </Box>
+  );
+};
