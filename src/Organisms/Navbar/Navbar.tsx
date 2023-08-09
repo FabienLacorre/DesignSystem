4500449977ed29a6440ca5ea4ae84bb87c7175a7
Fabien Lacorre <34058833+FabienLacorre@users.noreply.github.com>
2023-08-09 22:58:52 +0200

    feat: navbar

--- src/Organisms/Navbar/Navbar.tsx
+++ src/Organisms/Navbar/Navbar.tsx
@@ -2,6 +2,8 @@ import "./navbar.css";
 import clsx from "clsx";
 import { Box } from "../../Atoms/Box/Box";
 import { BasicProps } from "../../Atoms/_Types/basicProps";
+import { Typography } from "../../Atoms/Typography/Typography";
+import { Button } from "../../Molecules/Button/Button";
 
 interface NavbarProps extends BasicProps {}
 
@@ -11,8 +13,18 @@ export const Navbar = ({ className, style }: NavbarProps) => {
       padding="medium"
       style={style}
       className={clsx([className, "shadow--classic--black"])}
+      display="flex-row"
+      alignItems="center"
+      justifyContent="space-between"
     >
-      this is a navbar test
+      <Box>
+        <Button content="MwoneDesign" textSize="medium" borderless />
+      </Box>
+      <Box>
+        <Button content="Home" borderless />
+        <Button content="Content" borderless />
+        <Button content="About" borderless />
+      </Box>
     </Box>
   );
 };
